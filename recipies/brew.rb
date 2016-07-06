aefb6244643cb66e595666c14a5d8dc302176e13
839 <8398a7@gmail.com>
2016-07-06 23:20:54 +0900

    brew installをpackageに置換

--- recipies/brew.rb
+++ recipies/brew.rb
@@ -37,7 +37,7 @@ end
 # Install bin packages
 node['brew']['install_packages'].each do |package|
   execute 'Install bin packages' do
-    command "brew install #{package}"
+    package package
     not_if "brew list | grep -q #{package}"
   end
 end
