88aaedb0aa32387028aec05cf114e403cad68ed1
839 <8398a7@gmail.com>
2016-08-26 19:53:57 +0900

    [fish] use method when get branch name

--- fish/functions/git_current_branch.fish
+++ fish/functions/git_current_branch.fish
@@ -2,12 +2,11 @@ function git_current_branch
   if echo $PWD | grep -q -E '\.git(/.*)?$'
     return
   end
-  set -l symbolic_ref (git symbolic-ref HEAD 2> /dev/null)
-  if [ -z $symbolic_ref ]
+  set -l name (git_current_branch_name 2> /dev/null)
+  if [ -z $name ]
     return
   end
 
-  set -l name (basename $symbolic_ref)
   set -l gitdir (git rev-parse --git-dir 2> /dev/null)
   set -l st (git status 2> /dev/null)
 
