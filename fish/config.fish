f96862b4e5db4bf3d7bad448afdb711d369e90bf
839 <8398a7@gmail.com>
2016-08-26 20:56:30 +0900

    [fish] refactoring alias

--- fish/config.fish
+++ fish/config.fish
@@ -11,20 +11,17 @@ case Linux
   source $HOME/.zsh/.zshmain
 end
 
-# other
+# alias {{{
 alias vi /usr/local/bin/vim
 alias vim nvim
 alias py 'python'
 alias tailf 'tail -f'
 alias rmdocker 'docker rm (docker ps -aq)'
 
-alias ls 'ls -F --color'
-alias ll 'ls -l'
-alias la 'ls -a'
-alias lla 'ls -al'
 alias lr 'ls -R'
 alias rb 'ruby'
 
+# git {{{
 alias gst 'git status'
 alias gch 'git checkout'
 alias gbr 'git branch'
@@ -32,14 +29,18 @@ alias gpl 'git pull origin (git_current_branch_name)'
 alias gps 'git push origin (git_current_branch_name)'
 alias glg "git log --graph --pretty=format:'%Cred%h%Creset - %s %Cgreen(%cr) %C(bold blue)<%an>%Creset%C(yellow)%d%Creset' --abbrev-commit --date=relative"
 alias gla "git log --graph --all --pretty=format:'%Cred%h%Creset - %s %Cgreen(%cr) %C(bold blue)<%an>%Creset%C(yellow)%d%Creset' --abbrev-commit --date=relative"
-
+# }}}
+# tmux {{{
 alias at 'tmux attach -t'
 alias ks 'tmux kill-session -t'
 alias tls 'tmux ls'
 alias trs 'tmux rename -t'
-
+# }}}
+# rails {{{
 alias be 'bundle exec'
 alias rs 'rails s -b 0.0.0.0'
 alias rc 'rails c'
 alias pdep 'bundle exec cap production deploy'
 alias sdep 'bundle exec cap staging deploy'
+# }}}
+# }}}
