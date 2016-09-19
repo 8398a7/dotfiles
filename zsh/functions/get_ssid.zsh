9164e402b53aea68375a50ddfa25c346871c38d6
839 <8398a7@gmail.com>
2016-09-19 19:24:32 +0900

    [zsh] add get ssid function

---
+++ zsh/functions/get_ssid.zsh
@@ -0,0 +1,7 @@
+get_ssid() {
+  if echo $OSTYPE | grep -q darwin; then
+    /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep " SSID" | awk '{$1="";print}' | sed "s/ //"
+  else
+    echo Darwin Only
+  fi
+}
