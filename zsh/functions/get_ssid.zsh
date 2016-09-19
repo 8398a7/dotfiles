7c0d675115c786d4ce8abd6ebe929f9519ecce3c
839 <8398a7@gmail.com>
2016-09-19 19:30:39 +0900

    [zsh] remove messages

--- zsh/functions/get_ssid.zsh
+++ zsh/functions/get_ssid.zsh
@@ -2,6 +2,6 @@ get_ssid() {
   if echo $OSTYPE | grep -q darwin; then
     /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep " SSID" | awk '{$1="";print}' | sed "s/ //"
   else
-    echo Darwin Only
+    echo ""
   fi
 }
