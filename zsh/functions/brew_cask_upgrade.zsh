da150f479fd43d7023e2910b16571239fd2af2fb
839 <8398a7@gmail.com>
2017-02-02 00:53:50 +0900

    [zsh] fix brew cask upgrade

--- zsh/functions/brew_cask_upgrade.zsh
+++ zsh/functions/brew_cask_upgrade.zsh
@@ -1,10 +1,1 @@
-brew_cask_upgrade() {
-  if echo $OSTYPE | grep -q darwin; then
-    echo brew cask upgrade
-    for c in `brew cask list`; do ! brew cask info $c | grep -qF "Not installed" || brew cask install $c; done
-    echo brew cask cleanup
-    brew cask cleanup
-  else
-    echo not darwin
-  fi
-}
+brew_cask_upgrade() { for app in $(brew cask list); do local latest="$(brew cask info "${app}" | awk 'NR==1{print $2}')"; local versions=($(ls -1 "/usr/local/Caskroom/${app}/.metadata/")); local current=$(echo ${versions} | awk '{print $NF}'); if [[ "${latest}" = "latest" ]]; then echo "[!] ${app}: ${current} == ${latest}"; [[ "$1" = "-f" ]] && brew cask install "${app}" --force; continue; elif [[ "${current}" = "${latest}" ]]; then continue; fi; echo "[+] ${app}: ${current} -> ${latest}"; brew cask uninstall "${app}" --force; brew cask install "${app}"; done; }
