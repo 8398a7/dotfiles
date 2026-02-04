37da41f190ab0c0f0509cf201922e9409c57b662
839 <8398a7@gmail.com>
2026-02-04 16:47:59 +0900

    [zsh] sshconfigがなくても動作するようにした

--- zsh/functions/fzf_ssh.zsh
+++ zsh/functions/fzf_ssh.zsh
@@ -1,5 +1,5 @@
 fzf_ssh() {
-  local res=$(sshconfig | fzf --no-sort)
+  local res=$(awk '/^Host / && !/\*/ {print $2}' ~/.ssh/config | fzf --no-sort)
   if [ -n "$res" ]; then
     BUFFER+="ssh $res"
     zle accept-line
